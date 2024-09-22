ff4611a3f62de072d30bc34cd796587464ffe087
N Eashwar <neashwar05@gmail.com>
2024-09-22 16:10:18 +0530

    commit1

--- src/util/teams.js
+++ src/util/teams.js
@@ -1,7 +1,11 @@
 // src/util/teams.js
 export const initialTeams = [
-  { name: 'Team A', score: 50 },
-  { name: 'Team B', score: 40 },
-  { name: 'Team C', score: 60 },
+  { name: 'Maze-Craze', score: 0 },
+  { name: 'Labyrinth navigators', score: 0 },
+  { name: 'Tech Titans', score: 0 },
+  { name : 'Nadan Darindey',score:0},
+  { name : 'Maze Breaker',score:0},
+  { name : 'Gear Grinders',score:0},
+
   // Add more teams as needed
 ];
