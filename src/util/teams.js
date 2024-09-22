15e92256e89e73ea7287e7ae603695ba4674d175
Ved Sorathiya <sorathiyaved@gmail.com>
2024-09-22 16:29:33 +0530

    initial 2

--- src/util/teams.js
+++ src/util/teams.js
@@ -6,6 +6,14 @@ export const initialTeams = [
   { name : 'Nadan Darindey',time:0},
   { name : 'Maze Breaker',time:0},
   { name : 'Gear Grinders',time:0},
+  { name : 'Creepers',time:0},
+  { name : 'Skibidi Sigma',time:0},
+  { name : 'Dora the Explorer',time:0},
+  { name : 'Tism',time:0},
+
+
+
+
 
   // Add more teams as needed
 ];
