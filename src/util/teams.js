09e930bfb4ce01202a015e62ec6fff13a63fabe8
Ved Sorathiya <sorathiyaved@gmail.com>
2024-09-22 17:18:20 +0530

    initial one

--- src/util/teams.js
+++ src/util/teams.js
@@ -1,14 +1,14 @@
 export const initialTeams = [
   { name: 'Maze-Craze', time: 161082 }, // 1 hour in milliseconds 1.
-  // { name: 'Labyrinth navigators', time: 5400000 }, // 1.5 hours in milliseconds
+  { name: 'Labyrinth navigators', time: 137035 }, // 1.5 hours in milliseconds
   { name: 'Tech Titans', time: 167000 }, // 2 hours in milliseconds
   { name: 'Nadan Darindey', time: 161068 }, // 1.25 hours in milliseconds
   { name: 'Maze Breaker', time: 243000 }, // 50 minutes in milliseconds
   { name: 'Gear Grinders', time: 71000  }, // 1.67 hours in milliseconds
   // { name: 'Creepers', time: 98068 }, 
-  // { name: 'Skibdi Sigma', time: 98068 },
+  { name: 'Skibdi Sigma', time: 144044 },
   // { name: 'Dora The Explorer', time: 98068 },
-  // { name: '', time: 98068 },
+  { name: 'Soloman', time: 109072 },
   // { name: 'tism', time: 98068 },
 
 
